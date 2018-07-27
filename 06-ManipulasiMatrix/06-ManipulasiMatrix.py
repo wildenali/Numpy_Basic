ebfb1607d77151118566961ba140add234b6ee90
Wilden Ali <wildeeeeen@gmail.com>
2018-07-27 20:51:46 +0700

    Create 06-ManipulasiMatrix.py

---
+++ 06-ManipulasiMatrix/06-ManipulasiMatrix.py
@@ -0,0 +1,40 @@
+# Manipulasi matrix adalah jadi si matrix kita ubah berubah
+# misalnya awalnya 2x3, menjadi 3x2 (mengubah bentuknya yakan), dll
+# cekidot di bawah aja
+import numpy as np
+
+a = np.array((
+            [1,2,3],
+            [4,5,6]
+            ))
+
+print('matrix a dengan ukuran:', a.shape)
+print(a)
+
+
+# TRASPOSE -> mengubah bentuknya, misalnya dari 2x3 menjadi 3x2, contohnya seperti dibawah ini
+print('traspose matrix dari a:')
+print(a.transpose())    # Cara PERTAMA
+print(np.transpose(a))  # Cara KEDUA
+print(a.T)              # Cara KETIGA
+
+
+# FLATTEN -> MENSEJAJARKAN, menjadi vector baris
+print('flatten matrix a:')
+print(a.ravel())    # Hasilnya menjadi [1 2 3 4 5 6]
+print(np.ravel(a))  # Hasilnya menjadi [1 2 3 4 5 6]
+
+
+# RESHAPE -> Mengubah bentuk si matrixnya
+print('reShape matrix a:')
+print(a.reshape(3,2))
+print(a.reshape(6,1))
+
+
+# RESIZE -> Mengubah ukuran si matrixnya
+print('reSize matrix a:')
+a.resize(3,2)   # ini adalah cara Mengubah SIZE si matrix a
+print(a)
+print('matrix a berubah ukuran menjadi:', a.shape)
+# liat a.shape di baris 11, dengan a.shape di baris 38, bentuknya menjadi berubah
+# karena fungsi RESIZE adalah mengubah ukurannya okeeey
