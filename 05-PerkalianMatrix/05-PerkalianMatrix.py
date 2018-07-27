06674bf2a02d9c89e2638808a191e02772cfa565
Wilden Ali <wildeeeeen@gmail.com>
2018-07-27 20:51:12 +0700

    Create 05-PerkalianMatrix.py

---
+++ 05-PerkalianMatrix/05-PerkalianMatrix.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+# Biar ga lupa, perkalian matrix kaya gimana cuy
+"""
+a1 a2  *  b1 b2  =  (a1*b1)+(a2*b3)     (a1*b2)+(a2*b4)
+a3 a4     b3 b4     (a3*b1)+(a4*b3)     (a3*b2)+(a4*b4)
+"""
+
+a = np.array(([1,2],
+              [3,4]))
+b = np.ones([2,2])
+
+print("a: ")
+print(a)
+
+print("b: ")
+print(b)
+
+# PerKALIan matrix
+c = np.dot(a,b)
+
+print("a X b adalah")
+print(c)
+
+# Cara KEDUA
+d = a.dot(b)
+print("Cara ke DUA -> a X b adalah")
+print(d)
