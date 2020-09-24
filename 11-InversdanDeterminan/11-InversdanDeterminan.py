035ad722f33765d6c62fc37bb5e6c6ac76de23b0
wildenali <wildenwildenaliali@gmail.com>
2020-09-24 10:54:19 +0700

    give info for learn determine and invers matrix

--- 11-InversdanDeterminan/11-InversdanDeterminan.py
+++ 11-InversdanDeterminan/11-InversdanDeterminan.py
@@ -1,12 +1,14 @@
 import numpy as np
 
-a = np.array([(1,-1),(1,1)])
+# Belajar Invers dan Determinan dulu disini
+# https://idschool.net/sma/cara-menentukan-invers-determinan-matriks-dan-sifat-sifatnya/
+a = np.array([(1, -1), (1, 1)])
 print(a)
 
 # Inverse Matrix
 a_inv = np.linalg.inv(a)    # linalg -> liniear algebra
 print(a_inv)
-print(np.dot(a,a_inv))
+print(np.dot(a, a_inv))
 
 # DETERMINAN Matrix
 det_a = np.linalg.det(a)
