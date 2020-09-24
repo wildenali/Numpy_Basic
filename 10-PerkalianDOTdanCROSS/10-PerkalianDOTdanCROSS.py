cc965cbe5d2d59f067809e120c46ec0050e4f288
wildenali <wildenwildenaliali@gmail.com>
2020-09-24 10:39:23 +0700

    Penjelasan dot dan cross product

--- 10-PerkalianDOTdanCROSS/10-PerkalianDOTdanCROSS.py
+++ 10-PerkalianDOTdanCROSS/10-PerkalianDOTdanCROSS.py
@@ -1,17 +1,23 @@
 import numpy as np
 
-a = np.array([1,3])
-b = np.array([3,0])
+a = np.array([1, 3])
+b = np.array([3, 0])
+
+# Perkalian Dot
+# coba belajar dari sini dulu https://smatika.blogspot.com/2018/09/perkalian-titik-dot-product.html
 
 # perKALIan DOT
-c = np.dot(a,b)
+c = np.dot(a, b)
 print(c)
 
 
 # perKALIan CROSS
-d = np.array([1,2,0])
-e = np.array([2,1,0])
-f = np.cross(d,e)
-g = np.cross(e,d)
+# Coba pelajari untuk refresg tentang cross https://www.fisikabc.com/2017/06/perkalian-silang-dua-vektor.html
+
+# perKALIan CROSS
+d = np.array([1, 2, 0])
+e = np.array([2, 1, 0])
+f = np.cross(d, e)
+g = np.cross(e, d)
 print(f)
 print(g)
