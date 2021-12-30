62d9b4612fa2b3111f7aaae059159f3e5dff998a
rc-wilden <rc-wilden@dewatic.com>
2021-12-30 11:38:38 +0700

    fromfunction, fromiter

--- 08-AdvancedArray/08-AdvancedArray.py
+++ 08-AdvancedArray/08-AdvancedArray.py
@@ -2,7 +2,6 @@ import numpy as np
 
 # Membuat matrix dengan TIPE DATA tertentu, int, float, bool
 a = np.array(([1,2,3],[4,5,6]),dtype = float)
-print(a)
 
 
 # Membuat matrix dengan menggunakan FUNCTION
@@ -16,14 +15,12 @@ def jumlah(baris, kolom):
 # print(kuadrat(1,5))
 b = np.fromfunction(kuadrat, (1,10), dtype = int)
 c = np.fromfunction(jumlah, (4,4), dtype = float)
-print(b)
-print(c)
+cc = np.fromfunction(jumlah, (3,5), dtype = float)
 
 
 # Membuat Array atau Matrix dengan ITERABLE
 iterable = (x*x for x in range(5))
 d = np.fromiter(iterable, dtype = int)
-print(d)
 
 # MULTIPLE Array
 dtipe = [('nama','S255'), ('tinggi',int)]
@@ -32,7 +29,34 @@ data  = [   #ini adalah list
         ('otong', 160),
         ('mario', 180),
         ]
-print(data)
 
 e = np.array(data, dtype = dtipe)
-print(e)
+
+print('a\n', a)
+print('b\n', b)
+print('c\n', c)
+print('cc\n', cc)
+print('d\n', d)
+print('data\n', data)
+print('e\n', e)
+
+
+
+'''
+a
+ [[1. 2. 3.]
+ [4. 5. 6.]]
+b
+ [[ 0  1  4  9 16 25 36 49 64 81]]
+c
+ [[0. 1. 2. 3.]
+ [1. 2. 3. 4.]
+ [2. 3. 4. 5.]
+ [3. 4. 5. 6.]]
+d
+ [ 0  1  4  9 16]
+data
+ [('ucup', 150), ('otong', 160), ('mario', 180)]
+e
+ [(b'ucup', 150) (b'otong', 160) (b'mario', 180)]
+'''
