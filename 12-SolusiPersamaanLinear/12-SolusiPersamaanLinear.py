691a20ea18731e4ba6e4ab61dcb8170c09772e27
rc-wilden <rc-wilden@dewatic.com>
2021-12-30 12:17:56 +0700

    Persamaan Linear

--- 12-SolusiPersamaanLinear/12-SolusiPersamaanLinear.py
+++ 12-SolusiPersamaanLinear/12-SolusiPersamaanLinear.py
@@ -2,16 +2,26 @@ import numpy as np
 
 A = np.array([(2,3),(1,2)])
 Y = np.array([23,14])
-print(A)
-print(Y)
 
 A_inv = np.linalg.inv(A)
 
 # Menyelesaikan Persamaan LINEAR
 X = np.dot(A_inv,Y)
-print(X)
 
 
 # Cara Lain
 X2 = np.linalg.solve(A,Y)
+
+print(A)
+print(Y)
+print(X)
 print(X2)
+
+
+'''
+[[2 3]
+ [1 2]]
+[23 14]
+[4. 5.]
+[4. 5.]
+'''
