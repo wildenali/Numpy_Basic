e87c5b22cb024849a4cb6466996c05c4c6f357f4
rc-wilden <rc-wilden@dewatic.com>
2021-12-30 10:38:25 +0700

    ambil per element, misal posisi, awal sampe akhir, index sampe akhir, dll

--- 04-Index_Slice_Iteration/04-Index_Slice_Iteration.py
+++ 04-Index_Slice_Iteration/04-Index_Slice_Iteration.py
@@ -17,3 +17,25 @@ print('element dari AWAL sampai 5 dari a adalah', a[:5]) #[start:end]
 # ITERATION
 for i in a:
     print('nilai = ', i)
+
+
+'''
+[ 0  1  4  9 16 25 36 49 64 81]
+element ke 1 dari a adalah 0
+element ke 3 dari a adalah 1
+element ke 2 dari a adalah 4
+element ke 4 dari a adalah 9
+element dari 1-6 dari a adalah [ 0  1  4  9 16 25]
+element dari 4 sampai akhir dari a adalah [ 9 16 25 36 49 64 81]
+element dari AWAL sampai 5 dari a adalah [ 0  1  4  9 16]
+nilai =  0
+nilai =  1
+nilai =  4
+nilai =  9
+nilai =  16
+nilai =  25
+nilai =  36
+nilai =  49
+nilai =  64
+nilai =  81
+'''
