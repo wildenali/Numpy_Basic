2499e39b030d2602f32205959cb0a16f3466bc2a
rc-wilden <rc-wilden@dewatic.com>
2021-12-30 12:15:25 +0700

    Invers and Determinan

--- 11-InversdanDeterminan/11-InversdanDeterminan.py
+++ 11-InversdanDeterminan/11-InversdanDeterminan.py
@@ -3,19 +3,32 @@ import numpy as np
 # Belajar Invers dan Determinan dulu disini
 # https://idschool.net/sma/cara-menentukan-invers-determinan-matriks-dan-sifat-sifatnya/
 a = np.array([(1, -1), (1, 1)])
-print(a)
 
 # Inverse Matrix
 a_inv = np.linalg.inv(a)    # linalg -> liniear algebra
-print(a_inv)
-print(np.dot(a, a_inv))
 
 # DETERMINAN Matrix
 det_a = np.linalg.det(a)
 det_a_inv = np.linalg.det(a_inv)
-print(det_a)
-print(det_a_inv)
 
 # FUNGISNYA si INVERS dan DETERMINAN SALAH sATUNYA adalah
 # untuk
 # Menyelesaikan PERSAMAAN LINEAR
+
+print(a)
+print(a_inv)
+print(np.dot(a, a_inv))
+print(det_a)
+print(det_a_inv)
+
+
+'''
+[[ 1 -1]
+ [ 1  1]]
+[[ 0.5  0.5]
+ [-0.5  0.5]]
+[[1. 0.]
+ [0. 1.]]
+2.0
+0.5
+'''
