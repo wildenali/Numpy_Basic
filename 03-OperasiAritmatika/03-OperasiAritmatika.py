cbe49cb0d82af0debc9ca75b5b3a82b5ef16f5a9
rc-wilden <rc-wilden@dewatic.com>
2021-12-30 10:31:44 +0700

    result of aritmatics operation

--- 03-OperasiAritmatika/03-OperasiAritmatika.py
+++ 03-OperasiAritmatika/03-OperasiAritmatika.py
@@ -43,3 +43,17 @@ print(hasilBAGIarraynumpy)
 print(hasilKUADRATarraynumpy)
 print(hasilJUMLAHMULTIDIMENSI)
 print(hasilKALIMULTIDIMENSI)    # inget ini perkalian perelement, bukan perkalian matrix seperti matematika yg di pelajari okey, perkalian matrix kaya di pelajaran, di materi selanjutnya
+
+
+'''
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+[ 7  9 11 13 15]
+[-5 -5 -5 -5 -5]
+[ 6 14 24 36 50]
+[0.16666667 0.28571429 0.375      0.44444444 0.5       ]
+[ 1  4  9 16 25]
+[[ 8 10 12]
+ [ 3  3  3]]
+[[  7  16  27]
+ [ -4 -10 -18]]
+'''
