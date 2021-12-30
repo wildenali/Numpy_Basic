f861dcb43417a068d95c4b69a6a52618ac6cf2a7
rc-wilden <rc-wilden@dewatic.com>
2021-12-30 11:10:48 +0700

    stacking the matrix

--- 07-Stacking/07-Stacking.py
+++ 07-Stacking/07-Stacking.py
@@ -4,17 +4,20 @@ import numpy as np
 
 a = np.array([1,2,3])   # ini ARRAY
 b = np.array([4,5,6])   # ini ARRAY
-
 c = np.hstack((a,b))    # Horizontal Stack
-d = np.vstack((a,b))    # Horizontal Stack
-print(c)
-print(d)
-
+d = np.vstack((a,b))    # Vertical Stack
 
 aMatrik = np.zeros((2,3))  # ini MATRIX
 bMatrik = np.ones((2,3))   # ini MATRIX
 
 cMatrik = np.hstack((aMatrik,bMatrik))
 dMatrik = np.vstack((aMatrik,bMatrik))
-print(cMatrik)
-print(dMatrik)
+
+print('a', a)
+print('b', b)
+print('c', c)
+print('d', d)
+print('aMatrix\n', aMatrik)
+print('bMatrix\n', bMatrik)
+print('cMatrix\n', cMatrik)
+print('dMatrix\n', dMatrik)
